e95ff108fcfb21b44210636a72e07fa821633c3e
Cherwin Nooitmeer <cherwin@gmail.com>
2018-11-05 13:46:12 +0000

    Add multithreaded test script

---
+++ entrypoint.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import concurrent.futures
+import time
+import socket
+import sys
+
+def lookup_host():
+    print(socket.gethostbyname(sys.argv[1]))
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    while True:
+        executor.submit(lookup_host)
+        time.sleep(0.05)
